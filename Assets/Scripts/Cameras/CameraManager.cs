96d244efcd5b9e81d9e4adbe9d6f7ea82fec41bb
kai.yasuda <kai0603603@gmail.com>
2021-02-02 19:46:09 +0900

    AddCameraManager

---
+++ Assets/Scripts/Cameras/CameraManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    // カメラの切り替え
+    // どこにどのカメラを有効にするか
+    // どこに
+    // ・mainの全体を回転するカメラ
+    // ・ズームしたときのカメラ
+
+    // 全体を回転するカメラのポジションを作成
+
+    // Start is called before the first frame update
+    [SerializeField] Transform[] mainCameraTransforms = default;
+    int currentMainPosition;
+    Camera currentCamera;
+    Camera mainCamera;
+
+    public static CameraManager instance;
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        currentCamera = Camera.main;
+
+        currentMainPosition = 0;
+        currentCamera.transform.position = mainCameraTransforms[currentMainPosition].position;
+        currentCamera.transform.rotation = mainCameraTransforms[currentMainPosition].rotation;
+    }
+
+    public void TurnLeft()
+    {
+        currentMainPosition--;
+        if(currentMainPosition < 0)
+        {
+            currentMainPosition = mainCameraTransforms.Length - 1;
+        }
+        currentCamera.transform.position = mainCameraTransforms[currentMainPosition].position;
+        currentCamera.transform.rotation = mainCameraTransforms[currentMainPosition].rotation;
+    }
+
+    public void TurnRight()
+    {
+        currentMainPosition++;
+        if (currentMainPosition >= mainCameraTransforms.Length)
+        {
+            currentMainPosition = 0;
+        }
+        currentCamera.transform.position = mainCameraTransforms[currentMainPosition].position;
+        currentCamera.transform.rotation = mainCameraTransforms[currentMainPosition].rotation;
+    }
+
+    public void SetZoomCamera(Camera camera)
+    {
+
+        mainCamera.gameObject.SetActive(false);
+        camera.gameObject.SetActive(true);
+        currentCamera = camera;
+    }
+
+    public void TurnBack()
+    {
+        mainCamera.gameObject.SetActive(true);
+        currentCamera.gameObject.SetActive(false);
+        currentCamera = Camera.main;
+    }
+
+}
---
+++ Assets/Scripts/Cameras/CameraZoomObj.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomObj : MonoBehaviour
+{
+    // クリックしたら、用意してあるカメラに切り替える
+    [SerializeField] Camera zoomCamera;
+    public void OnClickThis()
+    {
+        Debug.Log("カメラ切り替え");
+        //zoomCamera.gameObject.SetActive(true);
+        CameraManager.instance.SetZoomCamera(zoomCamera);
+    }
+}
